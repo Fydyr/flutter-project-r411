1f1f72c8a485030feb8987c0508157836d625aec
kinator <behani.julien@gmx.fr>
2025-02-06 14:44:48 +0100

    Fix background

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -26,9 +26,10 @@ class _MyHomePageState extends State<MyHomePage> {
                 fit: BoxFit.cover
             )
         ),
+          height: MediaQuery.of(context).size.height,
+          width: MediaQuery.of(context).size.width,
 
-        child: Scaffold(
-        ),
+          child: Text("fdsf"),
       ),
     );
   }
