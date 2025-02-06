9c29d9a10e5de82f353d786f42b884d26d746fa6
Enzo <fournierenzo62@gmail.com>
2025-02-06 14:44:52 +0100

    add button_dart

---
+++ lib/widgets/button.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class button extends StatelessWidget {
+  const button({super.key, required this.color, required this.text, required this.onPressed, required this.width, required this.height});
+
+  final Color color;
+  final String text;
+  final VoidCallback onPressed;
+  final double width;
+  final double height;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+
+    );
+  }
+}
