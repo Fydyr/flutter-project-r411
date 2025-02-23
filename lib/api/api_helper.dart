7d073d9fd93682d38316b309145f1e1393ee90e4
Enzo <fournierenzo62@gmail.com>
2025-02-23 17:11:51 +0100

    update: add new get in api helper

--- lib/api/api_helper.dart
+++ lib/api/api_helper.dart
@@ -12,7 +12,67 @@ class ApiHelper {
   final Dio dio;
 
   void getPokemonCards(){
-    final url = 'http://88.173.86.254:37391/pokemon-cards/1';
+    final url = 'http://88.173.86.254:37391/pokemon-cards/';
+
+    final Future<Response> response = dio.get(url);
+    response.then((r) {
+      print(r.statusCode);
+
+      var data = r.data as List<dynamic>;
+      print(data);
+    });
+  }
+
+  void getPokemonCardId(int id){
+    final url = 'http://88.173.86.254:37391/pokemon-cards/$id';
+
+    final Future<Response> response = dio.get(url);
+    response.then((r) {
+      print(r.statusCode);
+
+      var data = r.data as Map<String, dynamic>;
+      print(data);
+    });
+  }
+
+  void getPokemonTypes(){
+    final url = 'http://88.173.86.254:37391/pokemon-types';
+
+    final Future<Response> response = dio.get(url);
+    response.then((r) {
+      print(r.statusCode);
+
+      var data = r.data as List<dynamic>;
+      print(data);
+    });
+  }
+
+  void getPokemonTypesId(int id) {
+    final url = 'http://88.173.86.254:37391/pokemon-types/$id';
+
+    final Future<Response> response = dio.get(url);
+    response.then((r) {
+      print(r.statusCode);
+
+      var data = r.data as Map<String, dynamic>;
+      print(data);
+    });
+  }
+
+  void getPokemonAttacks() {
+    final url = 'http://88.173.86.254:37391/pokemon-attacks';
+
+    final Future<Response> response = dio.get(url);
+    response.then((r) {
+      print(r.statusCode);
+
+      var data = r.data as List<dynamic>;
+      print(data);
+    });
+  }
+
+  void getPokemonAttacksId(int id) {
+    final url = 'http://88.173.86.254:37391/pokemon-attacks/$id';
 
     final Future<Response> response = dio.get(url);
     response.then((r) {
