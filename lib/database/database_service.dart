af0043e882e4851bf2710110475b9f79ea203742
Enzo <fournierenzo62@gmail.com>
2025-03-11 15:44:22 +0100

    delete: user database

--- lib/database/database_service.dart
+++ lib/database/database_service.dart
@@ -1,7 +1,6 @@
 import 'package:isar/isar.dart';
 import 'package:path_provider/path_provider.dart';
 import 'dart:io'; // Pour Directory
-import 'user_database/user_data.dart'; // Import du modèle User
 import 'pokemon_database/pokemon_data.dart'; // Import du modèle Pokémon
 import 'type_image/type_image_data.dart'; // import du modèle Type image
 import 'attack/attack_data.dart'; // import du modèle Attack
@@ -14,7 +13,7 @@ class DatabaseService {
     final Directory dir = await getApplicationDocumentsDirectory();
 
     isar = await Isar.open(
-      [UserDataSchema, PokemonDataSchema, TypeImageDataSchema, AttackDataSchema], // Ajouter ici toutes les tables
+      [PokemonDataSchema, TypeImageDataSchema, AttackDataSchema], // Ajouter ici toutes les tables
       directory: dir.path,
     );
   }
